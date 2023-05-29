f5172a11784afd56ebf3484d0c1828604cc467df
gleb-skobinsky <glebgytnik@gmail.com>
2023-05-30 00:41:14 +0300

    Animated sinusoid wave WIP

--- app/src/main/java/com/example/compose/composegpt/components/app.kt
+++ app/src/main/java/com/example/compose/composegpt/components/app.kt
@@ -1,7 +1,6 @@
 package com.example.compose.composegpt.components
 
 import androidx.compose.animation.core.withInfiniteAnimationFrameMillis
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -15,21 +14,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.example.compose.composegpt.theme.ComposeGPTTheme
-import kotlin.math.cos
-import kotlin.math.sin
+import kotlin.math.PI
+import kotlin.math.exp
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 @Composable
 fun App(viewModel: ViewModel, onAction: () -> Unit) {
@@ -56,12 +52,13 @@ val lightBlue = Color(173, 216, 230)
 
 @Composable
 private fun BoxScope.RecordingButton(onAction: () -> Unit) {
-    val density = LocalDensity.current
     Row(
         Modifier.padding(bottom = 24.dp)
             .align(Alignment.BottomCenter)
             .size(64.dp)
             .border(width = 1.dp, brush = SolidColor(lightBlue), shape = RoundedCornerShape(50))
+            .drawWaves()
+            /*
             .background(
                 Brush.radialGradient(
                     listOf(
@@ -71,111 +68,133 @@ private fun BoxScope.RecordingButton(onAction: () -> Unit) {
                 ),
                 RoundedCornerShape(50)
             )
+             */
             .clickable {
                 onAction()
             }
             .clip(RoundedCornerShape(50))
-            .composed {
-                val time by produceState(0f) {
-                    while (true) {
-                        withInfiniteAnimationFrameMillis {
-                            value = it / 100f
-                        }
-                    }
-                }
-                drawBehind {
-                    val radius = 32
-                    val innerRadius = 30
-                    drawPetalRotated(45.0 + time * 5, radius, innerRadius, density, firstPetalBrush)
-                    drawPetalRotated(80.0 - time * 10, radius, innerRadius, density, secondPetalBrush)
-                    drawPetalRotated(127.0 + time * 20, radius, innerRadius, density, thirdPetalBrush)
-                }
-            }
     ) {}
 }
 
-private fun DrawScope.drawPetal(
-    angle: Double,
-    radius: Int,
-    innerRadius: Int,
-    density: Density,
-    firstPetalBrush: Brush,
-) {
-    val curveTargetX1 = radius + sin(Math.toRadians(angle)) * innerRadius
-    val curveTargetY2 = radius + cos(Math.toRadians(angle)) * innerRadius
-    val pointX2 = radius - sin(Math.toRadians(angle)) * innerRadius
-    val pointY2 = radius - cos(Math.toRadians(angle)) * innerRadius
-    with(density) {
-        // drawRect(Color.Red, Offset(pointX2.dp.toPx(), pointY2.dp.toPx()), size = Size(2.dp.toPx(), 2.dp.toPx()))
-        // drawRect(Color.Red, topLeft = Offset(32.dp.toPx(), 32.dp.toPx()), size = Size(2.dp.toPx(), 2.dp.toPx()))
-        // drawRect(Color.Red, topLeft = Offset((pointX2 + 2).dp.toPx(), (pointY2 + 2).dp.toPx()), size = Size(20.dp.toPx(), 20.dp.toPx()))
-
-        val path = Path().apply {
-            moveTo(radius.dp.toPx(), radius.dp.toPx())
-            quadraticBezierTo((radius + 12).dp.toPx(), (radius + 24).dp.toPx(), curveTargetX1.dp.toPx(), curveTargetY2.dp.toPx())
-            quadraticBezierTo((radius + 24).dp.toPx(), (radius - 5).dp.toPx(), (radius + 3).dp.toPx(), (radius - 3).dp.toPx())
-            quadraticBezierTo((radius - 28).dp.toPx(), (radius - 3).dp.toPx(), pointX2.dp.toPx(), pointY2.dp.toPx())
-            quadraticBezierTo((radius - 10).dp.toPx(), (radius - 20).dp.toPx(), radius.dp.toPx(), radius.dp.toPx())
-            // cubicTo((radius + 18).dp.toPx(), radius.dp.toPx(), (radius - 16).dp.toPx(), (radius - 10).dp.toPx(), pointX2.dp.toPx(), pointY2.dp.toPx())
-            // relativeQuadraticBezierTo(10.dp.toPx(), 24.dp.toPx(), radius.dp.toPx(), radius.dp.toPx())
+fun Modifier.drawWaves() = composed {
+    val speedRatio = 1f
+    val time by produceState(0f) {
+        while (true) {
+            withInfiniteAnimationFrameMillis {
+                value = it / 1000f * speedRatio
+            }
         }
-        drawPath(
-            path = path,
-            brush = firstPetalBrush,
-            style = Fill
-        )
+    }
+    val sinSize = 6
+    val waves = 3
+    drawBehind {
+        val mean = size.width / 2
+        val pointsDistance = size.width / sinSize
+        val step = size.width / sinSize / 3
+        val xPoints = constructXPoints(sinSize, pointsDistance, time, mean)
+        for (point in xPoints) {
+            drawRect(
+                Color.Red,
+                point,
+                Size(2.dp.toPx(), 2.dp.toPx())
+            )
+        }
     }
 }
 
-// For simply rotated petals
-private fun DrawScope.drawPetalRotated(
-    angle: Double,
-    radius: Int,
-    innerRadius: Int,
-    density: Density,
-    firstPetalBrush: Brush,
-) {
-    with(density) {
-        val curve1StartX = radius.dp.toPx()
-        val curve1StartY = radius.dp.toPx()
-        val curve1ControlX = (radius + sin(Math.toRadians(angle - 30.0)) * (innerRadius - 14)).dp.toPx()
-        val curve1ControlY = (radius + cos(Math.toRadians(angle - 30.0)) * (innerRadius - 14)).dp.toPx()
-        val curve1TargetX = (radius + sin(Math.toRadians(angle)) * innerRadius).dp.toPx()
-        val curve1TargetY = (radius + cos(Math.toRadians(angle)) * innerRadius).dp.toPx()
+private fun constructXPoints(
+    sinSize: Int,
+    pointsDistance: Float,
+    time: Float,
+    mean: Float,
+): MutableList<Offset> {
+    val points = mutableListOf<Offset>()
+    for (i in 0 until sinSize) {
+        val xMin = pointsDistance * i
+        val offsetX = xMin + (time % 1 * pointsDistance)
+        val offsetY = Distributor.calculateY(offsetX, mean)
+        points.add(Offset(offsetX, offsetY))
+    }
+    points.add(0, Offset(0f, mean))
+    points.add(Offset(mean * 2, mean))
+    return points
+}
 
-        val curve2ControlX = (radius + sin(Math.toRadians(angle + 62.0)) * (innerRadius - 8)).dp.toPx()
-        val curve2ControlY = (radius + cos(Math.toRadians(angle + 62.0)) * (innerRadius - 8)).dp.toPx()
-        val curve2TargetX = (radius - sin(Math.toRadians(angle + 10.0)) * (innerRadius - 24)).dp.toPx()
-        val curve2TargetY = (radius - cos(Math.toRadians(angle + 10.0)) * (innerRadius - 24)).dp.toPx()
+/*
+private fun DrawScope.sinPath(xPoints: List<Float>, mean: Flo) {
+    val yPoints = mutableListOf<Float>()
 
-        val curve3ControlX = (radius - sin(Math.toRadians(angle + 45.0)) * (innerRadius - 12)).dp.toPx()
-        val curve3ControlY = (radius - cos(Math.toRadians(angle + 45.0)) * (innerRadius - 12)).dp.toPx()
-        val curve3TargetX = (radius - sin(Math.toRadians(angle)) * innerRadius).dp.toPx()
-        val curve3TargetY = (radius - cos(Math.toRadians(angle)) * innerRadius).dp.toPx()
-
-        val curve4ControlX = (radius - sin(Math.toRadians(angle - 20.0)) * (innerRadius - 6)).dp.toPx()
-        val curve4ControlY = (radius - cos(Math.toRadians(angle - 20.0)) * (innerRadius - 6)).dp.toPx()
-
-//        drawCurveDebug(curve1StartX, curve1StartY, curve1ControlX, curve1ControlY, curve1TargetX, curve1TargetY)
-//        drawCurveDebug(curve1TargetX, curve1TargetY, curve2ControlX, curve2ControlY, curve2TargetX, curve2TargetY)
-//        drawCurveDebug(curve2TargetX, curve2TargetY, curve3ControlX, curve3ControlY, curve3TargetX, curve3TargetY)
-//        drawCurveDebug(curve3TargetX, curve3TargetY, curve4ControlX, curve4ControlY, curve1StartX, curve1StartY)
-        // drawRect(Color.Red, topLeft = Offset(32.dp.toPx(), 32.dp.toPx()), size = Size(2.dp.toPx(), 2.dp.toPx()))
-        // drawRect(Color.Red, topLeft = Offset((pointX2 + 2).dp.toPx(), (pointY2 + 2).dp.toPx()), size = Size(20.dp.toPx(), 20.dp.toPx()))
-
-
-        val path = Path().apply {
-            moveTo(curve1StartX, curve1StartY)
-            quadraticBezierTo(curve1ControlX, curve1ControlY, curve1TargetX, curve1TargetY)
-            quadraticBezierTo(curve2ControlX, curve2ControlY, curve2TargetX, curve2TargetY)
-            quadraticBezierTo(curve3ControlX, curve3ControlY, curve3TargetX, curve3TargetY)
-            quadraticBezierTo(curve4ControlX, curve4ControlY, curve1StartX, curve1StartY)
+    for (i in 0..sinSize) {
+        val xOffset = offset + pointsDistance * i
+        val basis = Distributor.normalDistributionY(xOffset, mean).toFloat()
+        yPoints.add(basis + mean)
+    }
+    val strokePath = Path().apply {
+        moveTo(0f, mean)
+        for (i in yPoints.indices) {
+            val currentX = offset + i * pointsDistance
+            if (i == 0) {
+                val conX1 = currentX / 2f
+                val conX2 = currentX / 2f
+                val conY2 = yPoints[i]
+                cubicTo(
+                    x1 = conX1,
+                    y1 = mean,
+                    x2 = conX2,
+                    y2 = conY2,
+                    x3 = currentX,
+                    y3 = yPoints[i]
+                )
+            } else {
+                val previousX = offset + (i - 1) * pointsDistance
+                val conX1 = (previousX + currentX) / 2f
+                val conX2 = (previousX + currentX) / 2f
+                val conY1 = yPoints[i - 1]
+                val conY2 = yPoints[i]
+                cubicTo(
+                    x1 = conX1,
+                    y1 = conY1,
+                    x2 = conX2,
+                    y2 = conY2,
+                    x3 = currentX,
+                    y3 = yPoints[i]
+                )
+            }
         }
-        drawPath(
-            path = path,
-            brush = firstPetalBrush,
-            style = Fill
+    }
+    drawPath(
+        path = strokePath,
+        color = Color.Green,
+        style = Stroke(
+            width = 1.dp.toPx(),
+            cap = StrokeCap.Round
         )
+    )
+}
+ */
+
+object Distributor {
+    private var positive: Boolean = true
+
+    private fun Boolean.toInt() = when (this) {
+        true -> 1
+        false -> -1
+    }
+
+    fun normalDistributionY(x: Float, mean: Float): Double {
+        positive = !positive
+        val stdDev = mean / 3
+        val exponent = -0.5 * ((x - mean) / stdDev).pow(2)
+        val denominator = stdDev * sqrt(2 * PI)
+        return (mean - 10) * exp(exponent) / denominator * 100 * positive.toInt()
+    }
+
+    fun calculateY(x: Float, mean: Float): Float {
+        positive = !positive
+        val stdDev = mean / 3
+        val exponent = -0.5 * ((x - mean) / stdDev).pow(2)
+        val denominator = stdDev * sqrt(2 * PI)
+        return mean + ((mean - 10) * exp(exponent) / denominator * 100 * positive.toInt()).toFloat()
     }
 }
 
@@ -202,4 +221,6 @@ val thirdPetalBrush = Brush.linearGradient(
         Color.Green.copy(alpha = 0.5f)
     )
 )
+
+
 
